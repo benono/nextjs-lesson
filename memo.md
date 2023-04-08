d21812aa62ed49a7a6dd3145550296786899ea7e
b_ono <bentoki.1213@gmail.com>
2023-04-08 22:51:07 +0900

    initial commit

---
+++ memo.md
@@ -0,0 +1,11 @@
+## React で使った snippet
+
+rfce
+
+#### getStaticProps()
+
+→ ビルド時に fetch して DB から最新のデータを取得・レンダリングをする。
+・サーバーサイドで実行
+・pages のみで使用可能
+・npm run dev リクエストごとに実施
+・npm start ビルド時に実行される
---
+++ pages/posts/[id].js
@@ -0,0 +1,56 @@
+import Link from 'next/link';
+import Layout from '../../components/Layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export default function Post({ post }) {
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Layout title={post.title}>
+      <p className="m-4">
+        {'ID : '}
+        {post.id}
+      </p>
+      <p className="mb-8 text-xl font-bold">{post.title}</p>
+      <p className="px-10">{post.body}</p>
+      <Link href="/blog-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 mr-3"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
+            />
+          </svg>
+          <span>Back to blog-page</span>
+        </div>
+      </Link>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = await getAllPostIds();
+  return {
+    paths,
+    fallback: false, //別のidで検索されたらfalseを返す。
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { post: post } = await getPostData(params.id);
+  return {
+    props: {
+      post,
+    },
+  };
+}
